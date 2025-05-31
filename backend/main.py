59f0c6c919f00df7ec3637a993bb6ba246597841
ndacundaSGlobal-It <nicolas.dacunda@servicesglobal-it.com>
2025-05-31 15:01:06 -0300

    NewProyect_v2

---
+++ backend/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import mysql.connector
+import os
+
+app = FastAPI()
+
+DB_HOST = os.getenv("DB_HOST", "yamanote.proxy.rlwy.net")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "IzHqftUcXdEZFbPeYhlEFhCErHxWRZuF")
+DB_NAME = os.getenv("DB_NAME", "sepeyco")
+DB_PORT = os.getenv("DB_PORT", "51235")
+
+
+@app.get("/repuesto")
+def get_first_repuesto():
+    try:
+        conn = mysql.connector.connect(
+            host=DB_HOST,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            database=DB_NAME
+        )
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM repuestos LIMIT 1;")
+        repuesto = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return {"repuesto": repuesto}
+    except Exception as e:
+        return {"error": str(e)}
