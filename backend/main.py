f8b10074da3429a6ab1cd57495988b0c6f6f57c6
ndacundaSGlobal-It <nicolas.dacunda@servicesglobal-it.com>
2025-05-31 17:20:15 -0300

    cambio en BD

--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,7 @@ DB_HOST = os.getenv("DB_HOST", "yamanote.proxy.rlwy.net")
 DB_USER = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "IzHqftUcXdEZFbPeYhlEFhCErHxWRZuF")
 DB_NAME = os.getenv("DB_NAME", "sepeyco")
-DB_PORT = os.getenv("DB_PORT", "51235")
-
+DB_PORT = int(os.getenv("DB_PORT", "51235"))
 
 @app.get("/repuesto")
 def get_first_repuesto():
@@ -18,7 +17,8 @@ def get_first_repuesto():
             host=DB_HOST,
             user=DB_USER,
             password=DB_PASSWORD,
-            database=DB_NAME
+            database=DB_NAME,
+            port=DB_PORT
         )
         cursor = conn.cursor(dictionary=True)
         cursor.execute("SELECT * FROM repuestos LIMIT 1;")
